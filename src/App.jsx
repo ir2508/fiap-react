c3c14b6933e74c10006d02fd5fd4dcde955d779b
Igor Roberto <47528721+ir2508@users.noreply.github.com>
2026-03-25 14:42:50 +0000

    componente, props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Counter from "./components/Counter"
+
 function App({tool = "VUE"}) {
-    return <h1>{tool}</h1>
+    return (
+        <>
+            <h1>{tool}</h1>
+            <Counter />
+        </>
+    )
 }
 
 export default App
